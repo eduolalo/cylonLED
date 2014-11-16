0d9e45ae43a1bd79d2b9367a05de899f5f939919
Christian <christian@lastroom.mx>
2014-11-16 10:55:44 -0600

    led.js

---
+++ led.js
@@ -0,0 +1,19 @@
+var Cylon = require('..');
+
+Cylon.robot({
+    connection: {
+        name: 'raspi',
+        adaptor: 'raspi'
+    },
+    device: {
+        name: 'led',
+        driver: 'led',
+        pin: 11
+    },
+    work: function(item) {
+        every((1).second(),
+        function() {
+            item.led.toggle();
+        });
+    }
+}).start();
