316aa53c8e1c8540fad116c604c5f3333e0f9e8f
Christian <christian@lastroom.mx>
2014-11-16 12:50:27 -0600

    cylon

--- led.js
+++ led.js
@@ -11,31 +11,9 @@ Cylon.robot({
         pin: 11
     },
     work: function(me) {
-        var times = 0;
-        me.led.turnOff();
-        function initMotor() {
-            var speed = 0;
-            me.led.brightness(speed);
-            while(speed < 256) {
-                me.led.brightness(speed);
-                var i = 0;
-                while(i < 10000) {
-                    i++;
-                }
-                speed++;
-            }
-        }
-
-        var motorInterval = setInterval(function() {
-            console.log('Time: ',times);
-            if (times === 3) {
-                clearInterval(motorInterval);
-                me.led.turnOff();
-            } else {
-                initMotor();
-                times++;
-            }
-        }, (10).seconds());
+        return every(1..second(), function() {
+            return my.led.toggle();
+        });
 
     }
 }).start();
